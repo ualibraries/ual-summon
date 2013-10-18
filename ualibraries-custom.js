d021abff14ea9921f14b114882d1e1008ab4aa16
Michael Hagedon <m.d+github@hagedon.org>
2013-10-18 16:31:42 -0700

    Convert HTML string to jQuery. Minus the ng-* stuff.

--- ualibraries-custom.js
+++ ualibraries-custom.js
@@ -2,10 +2,21 @@ $(document).ready(function() {
 
   var text_to_insert = '<div class="ng-scope" dynamic-link="" ng-repeat="link in links.links" no-ellipsis="true" link-class="custom" is-internal="false" link="http://arizona.summon.serialssolutions.com/" click="link.click(link.type)" text="."><!-- ngSwitchWhen: true --><!-- ngSwitchDefault:  --><a href="http://arizona.summon.serialssolutions.com/" class="ng-scope ng-binding custom" ellipsis="" ng-switch-default="" ng-class="linkClass" ng-href="http://arizona.summon.serialssolutions.com/" target="_blank" ng-bind-html-unsafe="text"> Start Over </a></div>';
         
+  var $start_over_container = $('<div></div>')
+    .addClass('ng-scope')
+  ;
+  
+  var $start_over_link = $('<a>Start Over</a>')
+    .attr('href', 'http://arizona.summon.serialssolutions.com/')
+    .addClass('ng-scope ng-binding custom')
+    //.attr('target', '_blank')
+    .appendTo($start_over_container)
+  ;
+  
   $("style").append("div.rightBar {width: 350px; max-width: 350px; min-width: 350px;}");
   $("style").append("div.previewRightArrow {right: 350px;}");
  
   //var topic_link_to_insert = '<!-- GRW --> | <a class="sourceLink customPrimaryLie?<';
-  $(text_to_insert).insertBefore("div.chat");
+  $start_over_container.insertBefore("div.chat");
 
 });
