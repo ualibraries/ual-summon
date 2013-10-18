f31e0a41ac90ca67f3d5489a04a258209026c505
Ginger Bidwell <liquid06@gmail.com>
2013-10-18 15:45:23 -0700

    Initial commit for this test

---
+++ ualibraries-custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+$("div.Logo[size='small'] img").css('cursor', 'pointer').attr('title', 'UA Libraries homepage').attr('alt', 'UA Libraries homepage');
+    $("div.Logo[size='small'] img").click(function() {
+	window.location.href = 'http://www.library.arizona.edu';
+    });
+
+    
+    var text_to_insert = '<div class="ng-scope" dynamic-link="" ng-repeat="link in links.links" no-ellipsis="true" link-class="custom" is-internal="false" link="http://arizona.preview.summon.serialssolutions.com/" click="link.click(link.type)" text="."><!-- ngSwitchWhen: true --><!-- ngSwitchDefault:  --><a href="http://arizona.preview.summon.serialssolutions.com/" class="ng-scope ng-binding custom" ellipsis="" ng-switch-default="" ng-class="linkClass" ng-href="http://arizona.preview.summon.serialssolutions.com/" target="_blank" ng-bind-html-unsafe="text"> Start Over </a></div>
+        
+
+    $("style").append("div.rightBar {width: 350px; max-width: 350px; min-width: 350px;}");
+    $("style").append("div.previewRightArrow {right: 350px;}");
+ 
+    var topic_link_to_insert = '<!-- GRW --> | <a class="sourceLink customPrimaryLie?<
+    $(text_to_insert).insertBefore("div.ng-scope[link='www.library.arizona.edu/ask']");
+
+});
