8419f5e630bf2a92662ea6b69107f88c199fc0d6
blakistonr <blakistonr@u.library.arizona.edu>
2013-10-18 15:51:26 -0700

    Updated from preview to non-preview site

--- ualibraries-custom.js
+++ ualibraries-custom.js
@@ -6,7 +6,7 @@ $("div.Logo[size='small'] img").css('cursor', 'pointer').attr('title', 'UA Libra
     });
 
     
-    var text_to_insert = '<div class="ng-scope" dynamic-link="" ng-repeat="link in links.links" no-ellipsis="true" link-class="custom" is-internal="false" link="http://arizona.preview.summon.serialssolutions.com/" click="link.click(link.type)" text="."><!-- ngSwitchWhen: true --><!-- ngSwitchDefault:  --><a href="http://arizona.preview.summon.serialssolutions.com/" class="ng-scope ng-binding custom" ellipsis="" ng-switch-default="" ng-class="linkClass" ng-href="http://arizona.preview.summon.serialssolutions.com/" target="_blank" ng-bind-html-unsafe="text"> Start Over </a></div>
+    var text_to_insert = '<div class="ng-scope" dynamic-link="" ng-repeat="link in links.links" no-ellipsis="true" link-class="custom" is-internal="false" link="http://arizona.summon.serialssolutions.com/" click="link.click(link.type)" text="."><!-- ngSwitchWhen: true --><!-- ngSwitchDefault:  --><a href="http://arizona.summon.serialssolutions.com/" class="ng-scope ng-binding custom" ellipsis="" ng-switch-default="" ng-class="linkClass" ng-href="http://arizona.summon.serialssolutions.com/" target="_blank" ng-bind-html-unsafe="text"> Start Over </a></div>
         
 
     $("style").append("div.rightBar {width: 350px; max-width: 350px; min-width: 350px;}");
