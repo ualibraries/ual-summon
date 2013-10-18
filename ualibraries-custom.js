6b49aa04e9291f7578df062ac647918958d9013a
Michael Hagedon <m.d+github@hagedon.org>
2013-10-18 16:25:08 -0700

    Remove logo link.

--- ualibraries-custom.js
+++ ualibraries-custom.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-  $("div.Logo[size='small'] img").click(function() {
-    window.location.href = 'http://www.library.arizona.edu';
-  });
-    
   var text_to_insert = '<div class="ng-scope" dynamic-link="" ng-repeat="link in links.links" no-ellipsis="true" link-class="custom" is-internal="false" link="http://arizona.summon.serialssolutions.com/" click="link.click(link.type)" text="."><!-- ngSwitchWhen: true --><!-- ngSwitchDefault:  --><a href="http://arizona.summon.serialssolutions.com/" class="ng-scope ng-binding custom" ellipsis="" ng-switch-default="" ng-class="linkClass" ng-href="http://arizona.summon.serialssolutions.com/" target="_blank" ng-bind-html-unsafe="text"> Start Over </a></div>';
         
   $("style").append("div.rightBar {width: 350px; max-width: 350px; min-width: 350px;}");
