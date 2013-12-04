2b497c376a0d23ebc3c72865aee579676b9348d0
Gabriel Luethje <luethjeg@u.library.arizona.edu>
2013-12-04 14:11:17 -0700

    Version the damn CSS

--- ualibraries-custom.js
+++ ualibraries-custom.js
@@ -27,7 +27,7 @@ $('<style>'+
 /**
 * Write a link to our custom CSS file
 */
-$('<link rel="stylesheet" href="https://rawgithub.com/liquid06/summon/master/ualibraries-custom.css?ver=120413">').appendTo( $('head') );
+$('<link rel="stylesheet" href="https://rawgithub.com/liquid06/summon/master/ualibraries-custom.css?ver=120413-2">').appendTo( $('head') );
 
 /**
  * Change width of right sidebar.
