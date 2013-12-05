4996b908546c3d46fe77a62f7722c614aae916b1
Gabriel Luethje <luethjeg@u.library.arizona.edu>
2013-12-05 11:21:51 -0700

    version CSS

--- ualibraries-custom.js
+++ ualibraries-custom.js
@@ -34,7 +34,7 @@ if($alert_banner.css('display') == 'block'){
 /**
 * Write a link to our custom CSS file
 */
-$('<link rel="stylesheet" href="https://rawgithub.com/liquid06/summon/master/ualibraries-custom.css?ver=120413-13">').appendTo( $('head') );
+$('<link rel="stylesheet" href="https://rawgithub.com/liquid06/summon/master/ualibraries-custom.css?ver=120513-1">').appendTo( $('head') );
 
 /**
  * Change width of right sidebar.
