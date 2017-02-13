6a26944153352c54f9976e076af120a11d46d280
Christopher Green <cg433n@email.arizona.edu>
2017-02-13 11:03:56 -0700

    Adds LibChat button to hamburger menu Refs #9293

--- assets/scripts/ualibraries-custom.js
+++ assets/scripts/ualibraries-custom.js
@@ -1,43 +1,53 @@
 /*global $,angular*/
-$(document).ready(function() {
+$(document).ready(() => {
+
+  const libchatId = '#libchat_07713bc057f66ebcdccd4dd1b4a2be3e'
+  
+  const askUsLink = angular.element('<div class="ask-us-container"><a href="http://new.library.arizona.edu/contact" target="_blank" class="ask-us">Ask us</a></div>')
 
   /**
    * Summon search patch
    * We lose the search without this, and the Summon API crashes in Firefox after authentication.
    */
-  $('.vpnBanner a').click(function(e) {
+  $('.vpnBanner a').click((e) => {
     if (window.location.hash.length) {
-      e.preventDefault();
-      var summonPath = window.location.hash.substring(1).replace(/^!/, '');
-      summonPath = summonPath.replace('|', encodeURIComponent('|')); // fvf crashes the Summon API if there's a pipe in the Referer (Firefox only)
-      var loginURL = 'http://ezproxy.library.arizona.edu/login?url=http://arizona.summon.serialssolutions.com' + summonPath;
-      window.self.location = loginURL;
+      
+      e.preventDefault()
+      
+      let summonPath = window.location.hash.substring(1).replace(/^!/, '')
+      // fvf crashes the Summon API if there's a pipe in the Referer (Firefox only)
+      summonPath = summonPath.replace('|', encodeURIComponent('|')) 
+      
+      let loginURL = 'http://ezproxy.library.arizona.edu/login?url=http://arizona.summon.serialssolutions.com' + summonPath
+      
+      window.self.location = loginURL
     }
-  });
+  })
 
   /**
    * Write a link to our custom CSS file
    */
-  $('<link rel="stylesheet" href="//www.library.arizona.edu/vendor-support/summon/dist/styles/ualibraries-custom.css?ver=1478041665">').appendTo( $('head') );
+  $('<link rel="stylesheet" href="//www.library.arizona.edu/vendor-support/summon/dist/styles/ualibraries-custom.css?ver=1478041665">').appendTo( $('head') )
 
   /**
    * Add a link to Ask us on small screens
    */
-  var askUsLink = angular.element('<div class="ask-us-container"><a href="http://new.library.arizona.edu/contact" target="_blank" class="ask-us">Ask us</a></div>');
-  $('.content .holdingsOnlyToggle').after(askUsLink);
-  
+  $('.content .holdingsOnlyToggle').after(askUsLink)
+
   /**
    * Display a tooltip when the chat button is hovered over
    */
-  // We have to wait for the chat button to be added to the DOM
-  setTimeout(function() {
-    $('.libchat_online').after('<div class="tooltip bottom fade" style="top: 33px; left: -7px;"><div class="tooltip-arrow"></div><div class="tooltip-inner">Chat</div></div>');
-    
-    $('.libchat_online').hover(function () {
-      $('.libchat_online').next('.tooltip').addClass('in');
-    }, function() {
-      $('.libchat_online').next('.tooltip').removeClass('in');
-    });
-  }, 2000);
-  
-}); // end $(document).ready(function(){});
+  setTimeout(() => {
+    // We have to wait for the chat button to be added to the DOM
+    $('.libchat_online').after('<div class="tooltip bottom fade" style="top: 33px left: -7px"><div class="tooltip-arrow"></div><div class="tooltip-inner">Chat</div></div>')
+
+    $('.libchat_online').hover(() => {
+      $('.libchat_online').next('.tooltip').addClass('in')
+    }, () => {
+      $('.libchat_online').next('.tooltip').removeClass('in')
+    })
+  }, 2000)
+
+  $('.linksMenu .siteLinks').append(`<li><div class="btn-group btn-group-justified"><button id="${libchatId}">Chat</button></div></li>`)
+
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-var cssnano   = require('cssnano');
-var eslint    = require('gulp-eslint');
-var gulp      = require('gulp');
-var imagemin  = require('gulp-imagemin');
-var less      = require('gulp-less');
-var postcss   = require('gulp-postcss');
-var reporter  = require('postcss-reporter');
-var stylelint = require('stylelint');
-var uglify    = require('gulp-uglify');
-var pump      = require('pump');
+const babel     = require('gulp-babel')
+const cssnano   = require('cssnano')
+const eslint    = require('gulp-eslint')
+const gulp      = require('gulp')
+const imagemin  = require('gulp-imagemin')
+const less      = require('gulp-less')
+const postcss   = require('gulp-postcss')
+const reporter  = require('postcss-reporter')
+const stylelint = require('stylelint')
+const uglify    = require('gulp-uglify')
+const pump      = require('pump')
 
 // PostCSS processors
-var processors = [
+const processors = [
   stylelint({
     configFile: '.stylelintrc.json'
   }),
@@ -23,49 +24,44 @@ var processors = [
       browsers: ['> 1%']
     }
   })
-];
+]
 
 // Styles task
-gulp.task('styles', function() {
+gulp.task('styles', () => {
   gulp.src('assets/styles/ualibraries-custom.less')
     .pipe(less())
     .pipe(postcss(processors))
-    .pipe(gulp.dest('dist/styles'));
-});
+    .pipe(gulp.dest('dist/styles'))
+})
 
 // Scripts task
-gulp.task('scripts', function(cb) {
-  // main JS file
+gulp.task('scripts', (cb) => {
   pump([
     gulp.src('assets/scripts/ualibraries-custom.js'),
     eslint({
       configFile: '.eslintrc.json'
     }),
+    babel({
+      presets: ['es2015']
+    }),
     uglify(),
     gulp.dest('dist/scripts')
-  ], cb);
-    
-  // Lint the Gulpfile
-  gulp.src('gulpfile.js')
-    .pipe(eslint({
-      configFile: '.eslintrc.json'
-    }))
-    .pipe(eslint.format());
-});
+  ], cb)
+})
 
 // Images task
-gulp.task('images', function() {
+gulp.task('images', () => {
   gulp.src('assets/images/**/*.png')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'));
-});
+    .pipe(gulp.dest('dist/images'))
+})
 
 // Watch task
-gulp.task('watch', function() {
-  gulp.watch('assets/styles/**/*.less', ['styles']);
-  gulp.watch('assets/scripts/**/*.js', ['scripts']);
-  gulp.watch('assets/images/**/*.png', ['images']);
-});
+gulp.task('watch', () => {
+  gulp.watch('assets/styles/**/*.less', ['styles'])
+  gulp.watch('assets/scripts/**/*.js', ['scripts'])
+  gulp.watch('assets/images/**/*.png', ['images'])
+})
 
 // Default task
-gulp.task('default', ['styles', 'scripts', 'images']);
+gulp.task('default', ['styles', 'scripts', 'images'])
