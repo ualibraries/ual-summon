5814733de13c6dbd97e06f4dda3d414807d53bef
Christopher Green <cg433n@email.arizona.edu>
2017-02-13 11:06:12 -0700

    Adds LibChat button to hamburger menu Refs #9293

--- assets/scripts/ualibraries-custom.js
+++ assets/scripts/ualibraries-custom.js
@@ -1,7 +1,7 @@
 /*global $,angular*/
 $(document).ready(() => {
 
-  const libchatId = '#libchat_07713bc057f66ebcdccd4dd1b4a2be3e'
+  // const libchatId = '#libchat_07713bc057f66ebcdccd4dd1b4a2be3e'
   
   const askUsLink = angular.element('<div class="ask-us-container"><a href="http://new.library.arizona.edu/contact" target="_blank" class="ask-us">Ask us</a></div>')
 
@@ -48,6 +48,6 @@ $(document).ready(() => {
     })
   }, 2000)
 
-  $('.linksMenu .siteLinks').append(`<li><div class="btn-group btn-group-justified"><button id="${libchatId}">Chat</button></div></li>`)
+  // $('.linksMenu .siteLinks').append(`<li><div class="btn-group btn-group-justified"><button id="${libchatId}">Chat</button></div></li>`)
 
 })
